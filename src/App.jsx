59ab5296e92690b9f76882fe044c48864e3287b9
seteflo <u20191a458@upc.edu.pe>
2025-01-06 16:21:43 -0500

    Se creó la lista de criptomonedas, se agregó axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import viteLogo from '/vite.svg'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const API_URL = import.meta.env.VITE_API_URL
+
+  const [criptos, setCriptos] = useState()
+
+  useEffect(() => {
+    axios.get(`${API_URL}assets`)
+    .then(data => {setCriptos(data.data.data);
+    })
+    .catch(() =>{console.error('La petición falló')})
+  }, [])
+
+  if(!criptos) return <span>Cargando...</span>
 
   return (
-    <h1>HOLA MUNDO!</h1>
+    <>
+      <h1>Lista de criptomonedas</h1>
+      <ul>
+        {criptos.map(({id, name, priceUsd}) => (
+          <li key={id}>Nombre: {name} Precio: {priceUsd}</li>))
+        }
+      </ul>
+    </>
+    
   )
 }
 
