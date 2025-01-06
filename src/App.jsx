3117246c3abebd6f602007f41e5f179358162fea
seteflo <u20191a458@upc.edu.pe>
2025-01-06 14:01:32 -0500

    Mi primer commit del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1>HOLA MUNDO!</h1>
+  )
+}
+
+export default App
